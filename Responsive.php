96cf9bf90c69ebfc06a732f604eef05a8fc5404f
memorieswith32 <156398599+memorieswith32@users.noreply.github.com>
2024-01-13 22:50:26 +0530

    Update Responsive.php

--- Responsive.php
+++ Responsive.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Responsive</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="c1c2.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+ 
+  <style>
+ 
+  </style>
+</head>
+<body>
+</body>
+</html>
